738229645e8575a0624c6e38ce7d3deb9c59eeab
Manan Nahata <killgear0@gmail.com>
2020-09-25 16:38:02 +0530

    Add urls for app

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def top_tweets_list(request):
+    return render(request, 'app/top_tweets_list.html', {})
+
+def user_tweets_list(request):
+    return render(request, 'app/user_tweets_list.html', {})
+
+def auth(request):
+    pass
+
+def callback(request):
+    pass
+
+def sign_out(request):
+    pass
